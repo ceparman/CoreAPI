2865301a98e96e274fa995730c2e732e0abdde7a
craig <craig@eightcore>
2017-06-13 19:09:38 -0400

    update of tests

---
+++ tests/new-testthat/testfiles/user.R
@@ -0,0 +1,22 @@
+
+#good and bad password, no account
+
+coreUrl<-'http://experience.platformforscience.com'
+user<-"apitester"
+pwd<-"passtests"
+badpwd<-"Cepcep!"
+
+#r<-CoreAPI::authBasic(coreUrl,user,pwd)
+#CoreAPI::logOut(coreUrl,user,r$sessionInfo$jessionid)
+#authBasic(url,user,badpwd)
+
+
+
+#multi tenant user login
+
+muser<-"craig"
+mpwd<-"cepcepcep3"
+account<-"RM3"
+
+#authBasic(url,muser,mpwd,account)
+
---
+++ tests/new-testthat/testfiles/user2.R
@@ -0,0 +1,6 @@
+
+#good and bad password, no account
+
+coreUrl<-'http://experience.platformforscience.com'
+user<-"bioarray"
+pwd<-"corepass"
